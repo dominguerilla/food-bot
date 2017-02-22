4684e6fea4703fc9b75e32e1bde26e4d4e46f2cf
dominguerilla <dominguerilla@gmail.com>
2017-02-22 13:58:43 -0500

    Create auth.js

---
+++ auth.js
@@ -0,0 +1,5 @@
+var eatstreet_token = ''; // EATSTREET API TOKEN HERE
+var slackbot_token = ''; // SLACKBOT TOKEN HERE
+
+module.exports.eatstreet = eatstreet_token;
+module.exports.slackbot = slackbot_token;
