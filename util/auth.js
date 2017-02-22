f652eb771adfa64e34019579a7d85158aca69f1d
Carlos Dominguez <dominguerilla@gmail.com>
2017-02-22 16:25:06 -0500

    Redoing auth.js

--- util/auth.js
+++ util/auth.js
@@ -1,3 +1,5 @@
+// After adding your tokens, be sure to run 'git update-index --assume-unchanged auth.js'
+// If not done, every time you pull from the repo, the tokens will be overwritten.
 var eatstreet_token = ''; // EATSTREET API TOKEN HERE
 var slackbot_token = ''; // SLACKBOT TOKEN HERE
 
